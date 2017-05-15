ce951266324e423b8269085afe8777ed5b44877f
calcarmichael <callunclive@hotmail.com>
2017-05-15 22:28:38 +0100

    Sign In Screen - Start UI
    Starting on UI of sign in screen

--- Skate Demo/SignInViewController.swift
+++ Skate Demo/SignInViewController.swift
@@ -38,23 +38,23 @@ class SignInViewController: UIViewController, UIViewControllerTransitioningDeleg
         
         //Editing the text fields UI
         
-        emailTextField.backgroundColor = UIColor.clear
-        emailTextField.tintColor = UIColor.white
-        emailTextField.textColor = UIColor.white
-        emailTextField.attributedPlaceholder = NSAttributedString(string: emailTextField.placeholder!, attributes: [NSForegroundColorAttributeName: UIColor(white: 1.0, alpha: 0.6)])
-        let bottomLineEmail = CALayer()
-        bottomLineEmail.frame = CGRect(x: 0, y: 29, width: 1000, height: 0.6)
-        bottomLineEmail.backgroundColor = UIColor(red: 50/255, green: 50/255, blue: 25/255, alpha: 1).cgColor
-        emailTextField.layer.addSublayer(bottomLineEmail)
-        
-        passwordTextField.backgroundColor = UIColor.clear
-        passwordTextField.tintColor = UIColor.white
-        passwordTextField.textColor = UIColor.white
-        passwordTextField.attributedPlaceholder = NSAttributedString(string: passwordTextField.placeholder!, attributes: [NSForegroundColorAttributeName: UIColor(white: 1.0, alpha: 0.6)])
-        let bottomLinePassword = CALayer()
-        bottomLinePassword.frame = CGRect(x: 0, y: 29, width: 1000, height: 0.6)
-        bottomLinePassword.backgroundColor = UIColor(red: 50/255, green: 50/255, blue: 25/255, alpha: 1).cgColor
-        passwordTextField.layer.addSublayer(bottomLinePassword)
+//        emailTextField.backgroundColor = UIColor.clear
+//        emailTextField.tintColor = UIColor.white
+//        emailTextField.textColor = UIColor.white
+//        emailTextField.attributedPlaceholder = NSAttributedString(string: emailTextField.placeholder!, attributes: [NSForegroundColorAttributeName: UIColor(white: 1.0, alpha: 0.6)])
+//        let bottomLineEmail = CALayer()
+//        bottomLineEmail.frame = CGRect(x: 0, y: 29, width: 1000, height: 0.6)
+//        bottomLineEmail.backgroundColor = UIColor(red: 50/255, green: 50/255, blue: 25/255, alpha: 1).cgColor
+//        emailTextField.layer.addSublayer(bottomLineEmail)
+//        
+//        passwordTextField.backgroundColor = UIColor.clear
+//        passwordTextField.tintColor = UIColor.white
+//        passwordTextField.textColor = UIColor.white
+//        passwordTextField.attributedPlaceholder = NSAttributedString(string: passwordTextField.placeholder!, attributes: [NSForegroundColorAttributeName: UIColor(white: 1.0, alpha: 0.6)])
+//        let bottomLinePassword = CALayer()
+//        bottomLinePassword.frame = CGRect(x: 0, y: 29, width: 1000, height: 0.6)
+//        bottomLinePassword.backgroundColor = UIColor(red: 50/255, green: 50/255, blue: 25/255, alpha: 1).cgColor
+//        passwordTextField.layer.addSublayer(bottomLinePassword)
         
         //Button only works when text fields input
         
